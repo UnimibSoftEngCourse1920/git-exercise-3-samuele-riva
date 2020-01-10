afb112d9922750c10ff085eea091a57356abb853
samuele-riva <s.riva72@campus.unimib.it>
2020-01-10 19:40:21 +0100

    modifica assert.java

--- src/main/java/org/junit/Assert.java
+++ src/main/java/org/junit/Assert.java
@@ -1031,4 +1031,9 @@ public class Assert {
     private static String buildPrefix(String message) {
         return message != null && message.length() != 0 ? message + ": " : "";
     }
+    
+    public static <T> int assertGreaterThanInt(T i1, T i2, java.util.Comparator<T> comparator)
+    {
+        return comparator.compare(i1, i2);
+    }
 }
